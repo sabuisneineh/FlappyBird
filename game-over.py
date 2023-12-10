7ac46f71b41d0be094d5d9917d772468aa642913
Sarah Abuisneineh <143637016+sabuisneineh@users.noreply.github.com>
2023-12-10 15:12:18 -0600

    Update game-over.py

--- game-over.py
+++ game-over.py
@@ -32,9 +32,9 @@ pass_pipe = False
 
 
 #load images
-bg = pygame.image.load('img/bg.png')
-ground_img = pygame.image.load('img/ground.png')
-button_img = pygame.image.load('img/restart.png')
+bg = pygame.image.load('bg.png')
+ground_img = pygame.image.load('ground.png')
+button_img = pygame.image.load('restart.png')
 
 
 def draw_text(text, font, text_col, x, y):
@@ -58,7 +58,7 @@ class Bird(pygame.sprite.Sprite):
 		self.index = 0
 		self.counter = 0
 		for num in range(1, 4):
-			img = pygame.image.load(f'img/bird{num}.png')
+			img = pygame.image.load(f'bird1.png')
 			self.images.append(img)
 		self.image = self.images[self.index]
 		self.rect = self.image.get_rect()
@@ -105,7 +105,7 @@ class Bird(pygame.sprite.Sprite):
 class Pipe(pygame.sprite.Sprite):
 	def __init__(self, x, y, position):
 		pygame.sprite.Sprite.__init__(self)
-		self.image = pygame.image.load('img/pipe.png')
+		self.image = pygame.image.load('pipe.png')
 		self.rect = self.image.get_rect()
 		#position 1 is from the top, -1 is from the bottom
 		if position == 1:
